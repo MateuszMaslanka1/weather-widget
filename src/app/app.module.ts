ffff75913572e305d81e12942e08ea0b27033dfa
MateuszMaslanka1 <mateusz.maslanka1@outlook.com>
2021-12-03 23:38:46 +0100

    new style for weather widget

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WeatherWidgetComponent } from './home/weather-widget/weather-widget.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherWidgetComponent
   ],
   imports: [
     BrowserModule,
